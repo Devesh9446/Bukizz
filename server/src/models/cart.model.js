d81aa80cb8444a7073a52dca29e8c55b323e13f0
Devesh9446 <Srivastavadevesh43@gmail.com>
2024-02-27 05:20:25 +0530

    cart model made

---
+++ server/src/models/cart.model.js
@@ -0,0 +1,11 @@
+class cartModel{
+    constructor(totalAmount,productId,address){
+        this.totalAmount=totalAmount;
+        this.productId=productId;
+        this.address=address;
+    }
+}
+
+export {
+    cartModel,
+}
