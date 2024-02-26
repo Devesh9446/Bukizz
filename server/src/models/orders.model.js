fd34c727ace75b0df3557bcb50e23303bdab7408
Devesh9446 <Srivastavadevesh43@gmail.com>
2024-02-27 05:13:50 +0530

    orders model made

--- server/src/models/orders.model.js
+++ server/src/models/orders.model.js
@@ -1,1 +1,18 @@
- 
+class orderModel{
+    constructor(userId,orderDate,orderName,cartData,saleAmount,address,cartLength,status,reviewId,transactionId){
+        this.userId=userId;
+        this.orderDate=orderDate;
+        this.orderName=orderName;
+        this.totalAmount=totalAmount;
+        this.saleAmount=saleAmount;
+        this.address=address;
+        this.cartData=[];
+        this.cartLength=cartLength;
+        this.status=status;
+        this.reviewId=reviewId;
+        this.transactionId=transactionId;
+    }
+    addCartData(cartData){
+        cartData.push(cartData);
+    }
+ }
