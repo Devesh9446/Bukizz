a0f7ac1c99931a5faa88e69e4fe08d1a71f0381b
Devesh9446 <Srivastavadevesh43@gmail.com>
2024-02-27 05:09:19 +0530

    school model made

--- server/src/models/school.model.js
+++ server/src/models/school.model.js
@@ -1,1 +1,26 @@
- 
+class School{
+    constructor(name,address,city,state,board,pincode,contactNumber,email,website,logo,banner,aboutUs,mission,image,ourInspiration){
+        this.name=name;
+        this.address=address;
+        this.city=city;
+        this.state=state;
+        this.board=board;
+        this.pincode=pincode;
+        this.contactNumber=contactNumber;
+        this.email=email;
+        this.website=website;
+        this.logo=logo;
+        this.banner=banner;
+        this.aboutUs=aboutUs;
+        this.mission=mission;
+        this.image=image;
+        this.ourInspiration=ourInspiration;
+        this.productId=productId;
+    }
+
+    addProduct(id){
+        this.productId.push(id);
+    }
+}
+
+export default School;
