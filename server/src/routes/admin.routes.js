defb16b26c5704697f616efdd88ca6f20c6e94ea
Devesh9446 <Srivastavadevesh43@gmail.com>
2024-02-27 05:10:29 +0530

    routing done

--- server/src/routes/admin.routes.js
+++ server/src/routes/admin.routes.js
@@ -1,16 +1,28 @@
 import { Router } from "express";
 import {order,fetchDataByStatus} from "../controllers/order.controller.js"
-import { schoolAdd } from "../controllers/school.controller.js";
-import {category ,categoryAdd} from "../controllers/category.controller.js"
+import { schoolAdd,school } from "../controllers/school.controller.js";
+import {category} from "../controllers/category.controller.js"
+import { categoryAdd } from "../controllers/category.controller.js";
 import {upload} from "../middleware/multer.middleware.js"
+import { general_products } from "../controllers/general_product.controller.js";
+import {product} from "../controllers/product.controller.js"
 
 const router=Router();
-
 router.route("/orders").get(order);
-router.route("/orders/c/:/status").get(fetchDataByStatus);
-router.route("/school").post(schoolAdd);
+router.route("/school").get(school);
+router.route("/orders/c/:/status").get(fetchDataByStatus); 
 router.route("/category").get(category);
-router.route("/category").post(upload.single("Image"),categoryAdd);
-
+router.route("/generalPoducts").get(general_products)
+router.route("/products/c/:categoryId").get(product);
+router.route("/school").post(schoolAdd);
+router.route("/category").post(
+    upload.fields([
+        {
+            name:"Image", 
+            maxCount:1
+        }
+    ])
+,categoryAdd);
+ 
 export default router;
-  
+   
