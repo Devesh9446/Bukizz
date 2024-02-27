a4522b0d47f7252e06ffbaa17c7f138957fabe89
jigovind556 <12113078@nitkkr.ac.in>
2024-02-27 18:26:49 +0530

    added schools page to show all schlool

---
+++ client/src/pages/Schools/Schools.jsx
@@ -0,0 +1,140 @@
+import { Button, Img, Text } from 'components';
+import Model from 'components/Model/Model';
+import Pagination from 'components/Pagination/Pagination';
+import SearchBar from 'components/SearchBar/SearchBar';
+import TableViewer from 'components/TableViewer/TableViewer';
+import AddCategories from 'pages/Categories/AddCategories';
+import React, { useEffect, useState } from 'react';
+import { fetchApi } from 'utils/fetchApi';
+import { Toast } from 'utils/swal';
+
+function Schools() {
+    const [showAddSchoolsForm, setShowAddSchoolForm] = useState(false);
+    const [schoolsData, setSchoolsData] = useState([]);
+    const [query, setQuery] = useState('');
+    const [loading, setLoading] = useState(true);
+    const handleSort = () => { }
+
+    useEffect(() => {
+        getData();
+    }, [showAddSchoolsForm])
+
+    const getData = async () => {
+
+        try {
+            const resp = await fetchApi('/v1/admin/school');
+            console.log(resp);
+            if (resp.success) {
+                setSchoolsData(resp.data);
+                // Toast.fire({
+                //     icon: "success",
+                //     title: "Data fetched successfully",
+                // });
+                setLoading(false);
+            }
+
+        } catch (error) {
+            console.log("error in fetching data", error);
+            Toast.fire({
+                icon: "error",
+                title: "Mobile already linked with other account",
+            });
+        }
+    }
+
+    if (showAddSchoolsForm) {
+        return <Model
+            setShowModel={setShowAddSchoolForm}
+            title="Add Categories"
+            children={
+                <AddCategories setShowModel={setShowAddSchoolForm} />
+            }
+        />
+    }
+    return (
+        <div className="bg-gray-50_01 flex sm:flex-col md:flex-col flex-row font-gilroy gap-[30px] items-start mx-auto w-full">
+
+            <div className="flex-1 sm:h-[1056px]  md:mt-0 mt-6 md:px-5 relative w-full">
+                <div className="absolute flex flex-col gap-6 inset-x-[0] items-center justify-start mx-auto top-[1%] w-full">
+                    <div className="flex flex-row md:gap-10 items-end justify-between w-full">
+                        <Text
+                            className="mb-[5px] mt-4 sm:text-2xl md:text-[26px] text-[28px] text-blue_gray-900"
+                            size="txtGilroySemiBold28"
+                        >
+                            Schools
+                        </Text>
+                        <div className="    ">
+                            <Button
+                                className="cursor-pointer font-medium min-w-[161px] rounded-md text-base text-center"
+                                color="blue_A700_01"
+                                size="lg"
+                                variant="fill"
+                                onClick={() => { setShowAddSchoolForm(true) }}
+                            >
+                                Add Schools
+                            </Button>
+                        </div>
+
+
+                    </div>
+                    <div className="flex flex-col gap-[30px] items-start justify-start w-[98%] md:w-full">
+                        <SearchBar
+                            setQuery={setQuery}
+                            query={query}
+                            placeholder="Type something to search"
+                            handleSort={handleSort}
+                        />
+                        <TableViewer
+                            heading={[
+                                "Sr.No",
+                                "Logo",
+                                "Name",
+                                "City",
+                                "Email",
+                                "Website",
+                            ]}
+                            loading={loading}
+                        >
+                            <Row
+                                data={schoolsData}
+                            //   page={page}
+                            //   limit={limit}
+                            //   fetchData={fetchData}
+                            />
+                        </TableViewer>
+                        <Pagination page={1} totalPage={10} limit={10} setLimit={() => { }} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Schools;
+
+
+const Row = ({ data = [], page = 1, limit = 1, }) => {
+
+
+    return (
+        <>
+            {data.map((el, index) => (
+                <tr key={el.schoolId}>
+                    <td>{(page - 1) * limit + index + 1}</td>
+                    <td> <Img
+                        className="h-[60px] md:h-auto object-cover rounded-[3px] w-[60px]"
+                        src={el.logo || el.image}
+                        alt={`${el.name}'s Logo`}
+                    />
+                    </td>
+                    <td>{el.name}</td>
+                    <td>{el.city}</td>
+                    <td>{el.email}</td>
+                    <td><a href={el.website} target='blank'>{el.website}</a></td>
+
+                </tr>
+
+            ))}
+        </>
+    );
+};
--- server/src/routes/admin.routes.js
+++ server/src/routes/admin.routes.js
@@ -9,7 +9,7 @@ import {product} from "../controllers/product.controller.js"
 
 const router=Router();
 router.route("/orders").get(order);
-router.route("/school").get(school);
+router.route("/school").post(school);
 router.route("/orders/c/:/status").get(fetchDataByStatus); 
 router.route("/category").get(category);
 router.route("/generalPoducts").get(general_products)
