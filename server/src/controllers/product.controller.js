3bbcf1e3656c9695db94723a4a10416591dc7047
Devesh9446 <Srivastavadevesh43@gmail.com>
2024-02-27 05:31:37 +0530

    product controllers made

--- server/src/controllers/product.controller.js
+++ server/src/controllers/product.controller.js
@@ -3,17 +3,14 @@ import {asyncHandler} from '../utils/asyncHandler.js';
 import { apiResponse } from '../utils/apiResponse.js';
 import {app} from "../firebase.js"
 import {doc,collection,query,updateDoc,where} from 'firebase/firestore'
-import { Stream } from '../models/Stream.model.js';
-import { SetData } from '../models/SetData.model.js';
-import { ProductModel } from '../models/product.model.js';
 
 const product=asyncHandler(async(req,res)=>{
     const {id}=req.param;
     if(!id){ 
         throw new apiError(404,"category id is required");
     }
-    try{ 
-        const q=query(collection(app,"product"),where("categoryId","==",id));
+    try{
+        const q=query(collection(app,"products"),where("categoryId","==",id));
         const data=await getDoc(q);
         if(!data){
             res.status(200).json(new apiResponse(200,{},"no data found"));
@@ -24,76 +21,30 @@ const product=asyncHandler(async(req,res)=>{
     }
 })
 
-const productAdd=asyncHandler(async(req,res)=>{
-    const {name,description,categoryId,classId,board,stream,streamName,price,image,salePrice,sku}=req.body;
-    if(!(name||description||classId||stream||board||price||image||salePrice||sku)){
-        throw new apiError(400,"All fields required");
+const productStockUpdate=asyncHandler(async(req,res)=>{
+    const {productId,Updated_value}=req.body;
+    if(!productId){
+        throw new apiError(400,"product ID is required");
     }
-    if(!categoryId){
-        throw new apiError(400,"category Id is required");
+    if(!Updated_value){
+        throw new apiError(400,"Updated Vlaue is requred");
     }
-    if(!classId){
-        throw new apiError(400,"class Id is required");
+    if(!Updated_value<0){
+        throw new apiError(400,"Updated value cannot be negative");
     }
-    let newStream=null;
-    let newSet=null;
-    if(stream){
-        if(!streamName){
-            throw new apiError(400,"streamName is required");
-        }
-        newStream=new Stream(streamName,price,image,salePrice,sku);
-    }else{
-        newSet=new SetData(name,image,sku,price,salePrice);
-    }
+
     try{
-        const data=new ProductModel(name,description,price,categoryId,classId,board,newStream,retailerId,newSet,reviewIdList);
-        req.status(200).json(new apiError(200,data,"data stored successfully"));
-    }catch(error){  
+        const instance=doc(app,"category",productId);
+        const updated_data=await updateDoc(instance,{
+            stock:Updated_value,
+        })
+        res.status(200).json(new apiResponse(200,updated_data,"data updated successfully"))
+    }catch(error){
         throw new apiError(400,error);
     }
 })
 
-const productStockUpdate=asyncHandler(async(req,res)=>{
-    const {productId,newStock}=req.body;
-    if(productId){
-        throw new apiError(400,"productId is required");
-    }
-    try{
-        const docRef=doc(app,"products",productId)      
-        const updated_data=await updateDoc(docRef,{
-            
-        })
-        res.status(200).json(new apiResponse(200,updated_data,"data send succesfully"));   
-    }catch(error){
-        throw new apiError(404,error);
-    }
-    
-})
-
-const productRetailerAdd=asyncHandler(async(req,res)=>{
-    const {retailerId,productId}=req.body;
-    if(!retailerId){
-        throw new apiError(400,"retilerId is required");
-    }
-    if(!productId){
-        throw new apiError(400,"productId is required");
-    }
-    
-    try{
-        const docRef=doc(app,"products",productId)      
-        const updated_data=await updateDoc(docRef,{
-            retailers: firebase.firestore.FieldValue.arrayUnion(retailerId)
-        })
-        res.status(200).json(new apiResponse(200,updated_data,"data send succesfully"));   
-    }catch(error){
-        throw new apiError(404,error);
-    }
-
-})
-
 export {
     product,
-    productAdd,
     productStockUpdate,
-    productRetailerAdd,
 }
